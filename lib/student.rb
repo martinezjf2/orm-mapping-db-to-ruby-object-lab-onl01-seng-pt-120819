e4901fd51f7b31c66249af797481039c18993824
Jeffrey Martinez <martinezjf2@gmail.com>
2020-03-04 02:18:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -111,7 +111,13 @@ class Student
     DB[:conn].execute(sql).map { |row| self.new_from_db(row) }.first
   end
   
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE
+    
+    SQL
     
   end
   
